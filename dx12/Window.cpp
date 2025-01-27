7809f7dd9d8993d06559185de214404cfad4bc01
Dirk Zimmermann <me@dirkz.com>
2025-01-27 14:31:58 +0100

    Show a window (and can close it)

--- dx12/Window.cpp
+++ dx12/Window.cpp
@@ -5,6 +5,19 @@
 namespace zdx
 {
 
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT:
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
 int Window::WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
 {
     int screenWidth = GetSystemMetrics(SM_CXFULLSCREEN);
@@ -16,9 +29,36 @@ int Window::WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     int left = screenWidthQuarter;
     int right = 3 * screenWidthQuarter;
     int top = 0;
-    int bottom = screenHeightHalve;
+    int width = right - left;
+    int height = screenHeightHalve;
 
-    return 0;
+    const wchar_t CLASS_NAME[] = L"DX Vertex Pipeline";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"DX Vertex Pipeline", WS_OVERLAPPEDWINDOW, left, top,
+                               width, height, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nShowCmd);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) != 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return static_cast<char>(msg.wParam);
 }
 
 } // namespace zdx
