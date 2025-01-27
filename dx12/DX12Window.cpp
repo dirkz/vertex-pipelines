4dbbc07e2d8fdf4ae6c81ff30f7817b5d10696ca
Dirk Zimmermann <me@dirkz.com>
2025-01-27 16:44:22 +0100

    Enable debug layer

--- dx12/DX12Window.cpp
+++ dx12/DX12Window.cpp
@@ -6,6 +6,23 @@ namespace zdx
 void DX12Window::OnInit()
 {
     OutputDebugString(L"DX12Window::OnInit()\n");
+
+    UINT dxgiFactoryFlags = 0;
+
+#if defined(_DEBUG)
+    // Enable the debug layer (requires the Graphics Tools "optional feature").
+    // NOTE: Enabling the debug layer after device creation will invalidate the active device.
+    {
+        ComPtr<ID3D12Debug> debugController;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+        {
+            debugController->EnableDebugLayer();
+
+            // Enable additional debug layers.
+            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+        }
+    }
+#endif
 }
 
 } // namespace zdx
