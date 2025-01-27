309ec7afa6098938c2f3c5bcbc1c142250b07926
Dirk Zimmermann <me@dirkz.com>
2025-01-27 10:59:08 +0100

    Tons of boilerplate for Hello, world.

---
+++ dx12/main.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                    _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
+{
+    OutputDebugString(L"Hello, world!\n");
+}
