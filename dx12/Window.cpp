a0d75e4fe6b20486b3678825c7331c45397b801f
Dirk Zimmermann <me@dirkz.com>
2025-01-27 14:20:37 +0100

    Screen dimensions

--- dx12/Window.cpp
+++ dx12/Window.cpp
@@ -7,6 +7,17 @@ namespace zdx
 
 int Window::WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
 {
+    int screenWidth = GetSystemMetrics(SM_CXFULLSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYFULLSCREEN);
+
+    int screenWidthQuarter = screenWidth / 4;
+    int screenHeightHalve = screenHeight / 2;
+
+    int left = screenWidthQuarter;
+    int right = 3 * screenWidthQuarter;
+    int top = 0;
+    int bottom = screenHeightHalve;
+
     return 0;
 }
 
