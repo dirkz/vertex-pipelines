49a0cd1e952044213d8621f8367977e9a94de423
Dirk Zimmermann <me@dirkz.com>
2025-01-29 19:00:09 +0100

    DX utils in separate OBJECT library

---
+++ dx12/dxutil/stdafx.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <comdef.h>
+#include <wrl.h>
+
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <d3dx12.h>
+#include <dxgi1_6.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include <array>
+#include <chrono>
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <map>
+#include <memory>
+#include <numbers>
+#include <span>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX; // for XMFLOAT3 etc.
